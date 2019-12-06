6bf8b8b326d9f98797ec992fb3f2074840c03ea8
nahomtekeste <46280184+nahomtekeste@users.noreply.github.com>
2019-12-06 19:08:21 +0000

    Initial commit

---
+++ app/src/main/java/com/example/order_food_app/ScrollingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.order_food_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+    }
+}
